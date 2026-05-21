789aa54b2f42ef08f7d69dedf0346076980ffc0f
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-05-21 16:12:03 +0300

    Update __init__.py

---
+++ barbershop/app/api/routes/__init__.py
@@ -0,0 +1,7 @@
+from app.api.routes.booking import router as booking_router
+from app.api.routes.services import router as services_router
+from app.api.routes.masters import router as masters_router
+from app.api.routes.booked_slots import router as booked_slots_router
+from app.api.routes.admin_masters import router as admin_masters_router
+from app.api.routes.admin_services import router as admin_services_router
+from app.api.routes.admin_bookings import router as admin_bookings_router
