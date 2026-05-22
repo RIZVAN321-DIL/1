1269db2c8c8dd0d960e8dab49b5b1fe78430a4eb
RIZVAN321-DIL <rizvandilyaverovich@gmail.com>
2026-05-22 05:03:56 +0300

    Delete barbershop directory

--- barbershop/barbershop/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.client import Client
-from app.models.master import Master
-from app.models.service import Service
-from app.models.booking import Booking
-from app.models.review import Review
